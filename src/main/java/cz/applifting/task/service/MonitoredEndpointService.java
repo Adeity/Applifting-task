07b8b78c736b5b8ed5b6c5d3c27be41d7cbbfd6b
Patrik Pavelka <pavelpa2@fel.cvut.cz>
2022-05-17 17:06:37 +0200

    CRUD operations

--- src/main/java/cz/applifting/task/service/MonitoredEndpointService.java
+++ src/main/java/cz/applifting/task/service/MonitoredEndpointService.java
@@ -26,38 +26,67 @@ public class MonitoredEndpointService {
         this.monitoredEndpointDao = monitoredEndpointDao;
     }
 
+    /**
+     * Create Monitored Endpoint from user input dto
+     */
     @Transactional
-    public void createMonitoredEndpoint(MonitoredEndpointDto dto, User owner) {
+    public MonitoredEndpoint createMonitoredEndpoint(MonitoredEndpointDto dto, User owner) {
         NullParameterException.checkNullThrowNullParameter(dto.getMonitoredInterval(), "monitored interval");
         NullParameterException.checkNullThrowNullParameter(dto.getName(), "name");
         NullParameterException.checkNullThrowNullParameter(dto.getHttpMethodEnum(), "http method");
         NullParameterException.checkNullThrowNullParameter(dto.getUrl(), "url");
 
-        // create the business object from dto ere
+
+        // create the business object from dto
         MonitoredEndpoint monitoredEndpoint = new MonitoredEndpoint(dto);
+        // if it already exists, then set id to new object so existing entity gets updated
+        Optional<MonitoredEndpoint> monitoredEndpointOptional = monitoredEndpointDao.findByUrlAndOwner(dto.getUrl(), owner);
+        monitoredEndpointOptional.ifPresent(endpoint -> monitoredEndpoint.setId(endpoint.getId()));
 
         monitoredEndpoint.setOwner(owner);
         monitoredEndpointDao.save(monitoredEndpoint);
+        return monitoredEndpoint;
     }
 
+    /**
+     * Delete/deactivate the endpoint.
+     * @param url - url of endpoint
+     * @param owner - user owner of MonitoredEndpoint
+     */
+    @Transactional
+    public void deactivateMonitoredEndpoint(String url, User owner) {
+        Optional<MonitoredEndpoint> m = monitoredEndpointDao.findByUrlAndOwner(url, owner);
+        if (m.isPresent()) {
+            MonitoredEndpoint res = m.get();
+            res.setActive(false);
+            monitoredEndpointDao.save(res);
+        }
+        throw NotFoundException.create("MonitoredEndpoint", "url " + url);
+    }
+
+    /**
+     * GET all MonitoredEndpoints owned by authenticated user
+     */
     @PostFilter("authentication.getCredentials() == filterObject.owner.userAccessToken")
-    public List<MonitoredEndpoint> getByName(String name, User owner) {
+    public List<MonitoredEndpoint> findAll() {
+        return monitoredEndpointDao.findAll();
+    }
+
+
+    /**
+     * GET all MonitoredEndpoints owned by authenticated user with name @name
+     */
+    @PostFilter("authentication.getCredentials() == filterObject.owner.userAccessToken")
+    public List<MonitoredEndpoint> getByName(String name) {
         List<MonitoredEndpoint> res = monitoredEndpointDao.findByName(name);
         return res;
     }
 
-    public List<MonitoredEndpoint> getAllByOwner(User owner) {
-        List<MonitoredEndpoint> res = monitoredEndpointDao.findByOwner(owner);
-        return res;
-    }
-
-
-
-    @Transactional
-    public void deleteMonitoredEndpoint(MonitoredEndpointDto dto, User owner) {
-
-    }
-
+    /**
+     * Find MonitoredEndpoint by its url and owner
+     * @param url - url of MonitoredEndpoint
+     * @param owner - user owner of MonitoredEndpoint
+     */
     public MonitoredEndpoint getMonitoredEndpointByUrlAndOwner(String url, User owner) {
         Optional<MonitoredEndpoint> res = monitoredEndpointDao.findByUrlAndOwner(url, owner);
         if (res.isEmpty()) {

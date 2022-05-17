090a4bb90115acce9c146edb5a96d21baa746403
Patrik Pavelka <pavelpa2@fel.cvut.cz>
2022-05-17 20:53:53 +0200

    deleted unused import

--- src/main/java/cz/applifting/task/dao/MonitoringResultDao.java
+++ src/main/java/cz/applifting/task/dao/MonitoringResultDao.java
@@ -1,6 +1,5 @@
 package cz.applifting.task.dao;
 
-import cz.applifting.task.model.MonitoredEndpoint;
 import cz.applifting.task.model.MonitoringResult;
 import org.springframework.data.jpa.repository.JpaRepository;
 

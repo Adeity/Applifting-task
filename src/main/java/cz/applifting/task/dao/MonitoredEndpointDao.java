5ce98f1fb426551c99025a64d9d07c0ef18e15be
Patrik Pavelka <pavelpa2@fel.cvut.cz>
2022-05-17 13:22:42 +0200

    find by queries

--- src/main/java/cz/applifting/task/dao/MonitoredEndpointDao.java
+++ src/main/java/cz/applifting/task/dao/MonitoredEndpointDao.java
@@ -1,7 +1,14 @@
 package cz.applifting.task.dao;
 
 import cz.applifting.task.model.MonitoredEndpoint;
+import cz.applifting.task.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
 public interface MonitoredEndpointDao extends JpaRepository<MonitoredEndpoint, Integer> {
+    List<MonitoredEndpoint> findByName(String name);
+    List<MonitoredEndpoint> findByOwner(User owner);
+
+    Optional<MonitoredEndpoint> findByUrlAndOwner(String url, User owner);
 }
